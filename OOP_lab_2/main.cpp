4c12546e633210454024d36f61a448aa3fc6c691
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-09-29 09:29:26 +0300

    Initial Commit

---
+++ OOP_lab_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP_lab_2
+//
+//  Created by Andriy Kobetiak on 29.09.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
